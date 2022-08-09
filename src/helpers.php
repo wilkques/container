e94c66e1361ed8cabb0640dfe0fb2d24708d9549
wilkques <wilkques@gmail.com>
2022-08-09 18:12:10 +0800

    change function name

--- src/helpers.php
+++ src/helpers.php
@@ -1,11 +1,11 @@
 <?php
 
-if (!function_exists('app_container')) {
+if (!function_exists('container')) {
     /**
      * @param string $abstruct
      * @param array $params
      */
-    function app_container($abstruct = null, $params = null)
+    function container($abstruct = null, $params = null)
     {
         if (is_null($abstruct)) {
             return \Wilkques\Container\Container::getInstance();
